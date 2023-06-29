5068f9a2ed347148ca0dd0790fe0c2ffafd78732
hakandereli <hakandereli28@gmail.com>
2023-06-29 13:27:40 +0300

    web service concepts explained part 1

---
+++ src/main/java/org/hakandereli/restful/_01/notes/Notes.java
@@ -0,0 +1,45 @@
+package org.hakandereli.restful._01.notes;
+
+public class Notes {
+}
+
+
+/**
+ * WEB SERVICE:
+ * Interoperating (birlikte çalışma) kavramından ortaya çıkmıştır.
+ * Bir uygulamamız var java da yazılmış olsun bir tane de python da bir tane de .net de bu 3 uygulamanın haberleşmesi
+ * web serviceler ile yapılabilir.
+ *
+ * İlla farklı programlama dili olmalı değil bir dilde eski sürümde yazılmış olabilir farklı yapıları kullanıyor olabilir.
+ * Kısaca farklı uygulamaların yada yapıların HTTTP veya SOAP protokolü ile haberleşmesidir.
+ *
+ * Yada dışarıya public bir hizmet açılabilir. Twitter, amazon vs gibi servisler örneğin.
+ *
+ *
+ *
+ * Java da 2 Tip Web Servis vardır;
+ * 1- JAX-RS
+ * 2- JAX-WS
+ *
+ * Bunlar Java EE bir parçasıdır. Spring de ise bu yapı baz alınarak kurulmuş bir frameworktur.
+ *
+ * ###
+ * JAX-RS
+ *  Java API for RESTful web services
+ *
+ *
+ *  JAX-RS implementations
+ *  1-Jersey
+ *  2-RESTeasy
+ *  3-Restlet
+ *  4-Apache CXF
+ *
+ *  ####
+ *  REST
+ *
+ *  2000 yılı , Roy Fielding , doktora tezinden ortaya çıkmıştır.
+ *
+ *  Representation State Transfer anlamında gelmektedir.
+ *
+ *  HTTP - bir tcp protokülüdür. Alıcı verici arasındaki iletişimi sağlar.
+ * */
